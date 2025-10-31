2fcfdc0afde27e7a00431eb816ed253c67198ade
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-31 10:54:38 +0100

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,16 @@
+console.clear();
+
+let result = 0;
+
+function add(a, b) {
+  return (result = a + b);
+}
+function subtract(a, b) {
+  return (result = a - b);
+}
+function multiply(a, b) {
+  return (result = a * b);
+}
+function divide(a, b) {
+  return (result = a / b);
+}
