343db5b70b855b9ec6a96d2aeb6dab256affd3e9
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-31 11:13:58 +0100

    Create variables for each part of the operation

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 console.clear();
 
+// variables
+let num1 = ``;
+let num2 = ``;
+let operator = ``;
 let result = 0;
 
+// basic math functions
 function add(a, b) {
   return (result = a + b);
 }
