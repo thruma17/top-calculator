e58f1d2b7007baf25ea4ce3fcdc37f78342782bc
thruma17-midon <210378036+thruma17@users.noreply.github.com>
2025-10-31 11:19:15 +0100

    Create switch to choose correct math function based on operator

--- script.js
+++ script.js
@@ -25,4 +25,21 @@ function operate(num1, operator, num2) {
   num1 = prompt(`Insert first number`, ``);
   operator = prompt(`Insert operator`, ``);
   num2 = prompt(`Insert second number`, ``);
+
+  switch (operator) {
+    case `+`:
+      add(num1, num2);
+      break;
+    case `-`:
+      subtract(num1, num2);
+      break;
+    case `*`:
+      multiply(num1, num2);
+      break;
+    case `/`:
+      divide(num1, num2);
+      break;
+  }
+
+  alert(result);
 }
